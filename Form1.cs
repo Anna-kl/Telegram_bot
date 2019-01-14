cd2171487fd8080fcbf5329e76e2f2488f9ea5f8
Anna-kl <38497039+Anna-kl@users.noreply.github.com>
2019-01-14 10:52:52 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TeleSharp.TL;
+using TeleSharp.TL.Channels;
+using TeleSharp.TL.Messages;
+using TLSharp;
+using TLSharp.Core;
+using TLSharp.Core.Network;
+using TLSharp.Core.Requests;
+using TLSharp.Core.Utils;
+
+namespace telegram_test
+{
+    public partial class Form1 : Form
+    {
+        TelegramClient client;
+        string hash;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //int unixTime = (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
+
+            //TimeSpan hours = new TimeSpan(-1, 0, 0);
+            //var time = DateTime.Now.Add(hours);
+           // Message mes = new Message();
+           // var telephone = GetChatForNumber("+79951209052");
+         
+           //var dictionaty_chat = mes.GetLastIdMessage(telephone, "+79951209052");
+          
+           //     mes.GetReplyDictionary(dictionaty_chat);
+                    
+            //mes.GetLastIdMessage("DataLight stAff");
+       
+            var store = new FileSessionStore();
+            client = new TelegramClient(632369, "7cb0f9a3d2e5108a4b9a961b86fd564a", store, "session3");
+
+            await client.ConnectAsync();
+            hash = await client.SendCodeRequestAsync("+79951209052");
+
+        }
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+        private Dictionary<int, string> GetChatForNumber(string number)
+        {
+            Dictionary<int, string> telethone = new Dictionary<int, string>();
+            string connString = "Host=localhost;Username=postgres;Password=2537300;Database=postgres";
+            string select = "SELECT  name_chat FROM public.bot_number where telethon=\'" + number + "\';";
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                // Insert some data
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+
+                    //cmd.Parameters.AddWithValue("p", "Hello world");
+                    NpgsqlCommand command = new NpgsqlCommand(select, conn);
+                    NpgsqlDataReader dr = command.ExecuteReader();
+                    int key = 0;
+                    try
+                    {
+                        while (dr.Read())
+                        {
+
+                            telethone.Add(key, dr[0].ToString());
+                            key++;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        return null;
+                    }
+                }
+                return telethone;
+            }
+        }
+
+            private async void button2_Click_1(object sender, EventArgs e)
+            {
+                string code = textBox1.Text;
+
+                var user = await client.MakeAuthAsync("+79951209052", hash, code);
+                Message mes = new Message();
+            var telephone = GetChatForNumber("+79951209052");
+            var dictionaty_chat = mes.GetLastIdMessage(telephone, "+79951209052");
+            foreach (var chat in dictionaty_chat)
+            {
+                var data = await GetChannel("DataLight stAff");
+                mes.GetLastMessage(data, client, chat.Key, chat.Value);
+                //var reply=mes.GetReplyDictionary()
+            }
+
+                //find channel by title
+                //            var chat = dialogs.Chats
+                //              .Where(c => c.GetType() == typeof(TLChannel))
+                //              .Cast<TLChannel>()
+                //              .FirstOrDefault(c => c.Title == "Крипто-скам-чат");
+                //            var chat_send = dialogs.Chats
+                //              .Where(c => c.GetType() == typeof(TLChannel))
+                //              .Cast<TLChannel>()
+                //              .FirstOrDefault(c => c.Title == "DataLight stAff");
+                //            var tlAbsMessages =
+                //                        await client.GetHistoryAsync(
+                //                            new TLInputPeerChannel { ChannelId = chat.Id, AccessHash=chat.AccessHash.Value }, 11000,
+                //                            10000000, 50);
+                //            TLChannelMessages res = await client.SendRequestAsync<TLChannelMessages>
+                //(new TLRequestGetHistory()
+                //{
+                //    Peer = new TLInputPeerChannel { ChannelId = chat.Id, AccessHash = chat.AccessHash.Value },
+                //    Limit = 400,
+                //    AddOffset = 100,
+                //    OffsetId = 0
+                //});
+                //            var msgs = res.Messages;
+
+                //            foreach (var message in msgs)
+                //            {
+                //                string text = message.ToString();
+
+                //                await client.SendMessageAsync(new TLInputPeerChannel() { ChannelId = chat_send.Id, AccessHash = chat_send.AccessHash.Value }, text);
+                //            }
+                //      var dialogs = (TeleSharp.TL.Messages.TLDialogs)await client.GetUserDialogsAsync();
+
+                //      var main_u = new TLUser();
+
+                //      var found = await client.SearchUserAsync("chestnykh", 1);
+                //      var u = found.Users
+                //.Where(x => x.GetType() == typeof(TLUser))
+                //.Cast<TLUser>().FirstOrDefault(x => x.FirstName == "Danila");
+                //      var ur = dialogs.Users.Where(x => x.GetType() == typeof(TLUser))
+                //.Cast<TLUser>().FirstOrDefault(x => x.FirstName == "Аня");
+                //            var dialogs = (TeleSharp.TL.Messages.TLDialogs)await client.GetUserDialogsAsync();
+                //            foreach (var element in dialogs.Chats)
+                //            {
+                //                await client.SendMessageAsync(new TLInputPeerChannel() { ChannelId = element., AccessHash = chat.AccessHash.Value }, "OUR_MESSAGE");
+                //                if (element is TLChat)
+                //                {
+                //                    TLChat chat = element as TLChat;
+
+                //                    //find channel by title
+                //                    TeleSharp.TL.Messages.TLChatFull channelInfo = await client.SendRequestAsync<TeleSharp.TL.Messages.TLChatFull>
+                //(new TeleSharp.TL.Messages.TLRequestGetFullChat() { ChatId = chat.Id });
+
+                //                    if (element is TLChannel)
+                //                    {
+                //                        var offset = 0;
+                //                        TLChannel channel = element as TLChannel;
+                //                        var chan = await client.SendRequestAsync<TeleSharp.TL.Messages.TLChatFull>(new TLRequestGetFullChannel()
+                //                        {
+                //                            Channel = new TLInputChannel()
+                //                            {
+                //                                ChannelId = channel.Id,
+                //                                AccessHash = (long)channel.AccessHash
+                //                            }
+                //                        });
+                //                        TLInputPeerChannel inputPeer = new TLInputPeerChannel()
+                //                        {
+                //                            ChannelId = channel.Id,
+                //                            AccessHash = (long)channel.AccessHash
+                //                        };
+                //                        await client.SendMessageAsync(inputPeer, "text message");
+
+                //                    }
+                //                }
+                //                //send message
+                //                //await client.SendMessageAsync(new TLInputPeerChannel() { ChannelId = chat.Id, AccessHash = chat.AccessHash.Value }, "OUR_MESSAGE");
+                //            }
+
+                //            if (users != null)
+                //            {
+                //                await client.SendMessageAsync(new TLInputPeerUser() { UserId = users.Id }, "Hi there!");
+                //            }
+
+                //            await client.SendMessageAsync(new TLInputPeerUser() { UserId = users.Id }, "OUR_MESSAGE");
+            }
+            private async Task<TLChannel> GetChannel(string name_channel)
+            {
+                var dialogs = (TLDialogs)await client.GetUserDialogsAsync();
+
+                //find channel by title
+                var chat = dialogs.Chats
+                  .Where(c => c.GetType() == typeof(TLChannel))
+                  .Cast<TLChannel>()
+                  .FirstOrDefault(c => c.Title == name_channel);
+                return chat;
+            }
+
+
+        }
+
+
+    } 
---
+++ Message.cs
@@ -0,0 +1,322 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeleSharp.TL;
+using TeleSharp.TL.Channels;
+using TeleSharp.TL.Messages;
+using TLSharp;
+using TLSharp.Core;
+using System.IO;
+using System.Windows.Forms;
+
+
+
+namespace telegram_test
+{
+
+    class Message
+    {
+        string connString = "Host=localhost;Username=postgres;Password=2537300;Database=postgres";
+        TLMessage message;
+        public void WriteMessageChannel(string name_chat, int Id_message)
+        {
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                // Insert some data
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT from_id, id_message, name_chat, message, \"date\" FROM telegram.message Limit 10 ";
+                    //cmd.Parameters.AddWithValue("p", "Hello world");
+                    NpgsqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        try
+                        {
+                            string result = reader.GetString(1);//Получаем значение из второго столбца! Первый это (0)!
+                        }
+                        catch { }
+
+                    }
+                    conn.Close();
+
+                }
+
+                // Retrieve all rows
+
+            }
+
+        }
+
+        public static int DateTimeToUnixTimeStamp(DateTime time)
+        {
+            int unixTime = (int)(time - new DateTime(1970, 1, 1)).TotalSeconds;
+            return unixTime;
+        }
+        int unixTime = (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+        public async void GetLastMessage(TLChannel channel, TelegramClient client, string name_chat, int offsetId)
+        {
+            int temp_offset = 0;
+            int now_time=0;
+            if (offsetId == 0)
+            {
+                TimeSpan hours = new TimeSpan(-1, 0, 0);
+                DateTime time = DateTime.Now.Add(hours);
+                now_time = DateTimeToUnixTimeStamp(time);
+            }
+            while (true)
+            {
+                TLChannelMessages res = await client.SendRequestAsync<TLChannelMessages>
+    (new TLRequestGetHistory()
+    {
+        Peer = new TLInputPeerChannel { ChannelId = channel.Id, AccessHash = channel.AccessHash.Value },
+        Limit = 100,
+        AddOffset = temp_offset,
+        OffsetId = 0
+    });
+
+                temp_offset += 100;
+
+                var msgs = (TLChannelMessages)res;
+
+                foreach (TLAbsMessage message in msgs.Messages)
+                {
+                    var tlMessage = message as TLMessage;
+                    if (tlMessage == null)
+                        continue;
+                    if (offsetId == 0)
+                    {
+
+                        if (tlMessage.Date < now_time)
+                        {
+                            break;
+                        }
+
+                    }
+                    else
+                    {
+                        if (tlMessage.Id < offsetId)
+                            break;
+                    }
+                    int replyId = -1;
+                    if (tlMessage.ReplyToMsgId != null)
+                    {
+                        replyId = Convert.ToInt16(tlMessage.ReplyToMsgId);
+                    }
+                    string insert = "INSERT INTO public.message(messageid, fromid, chatname, processing_dttm, message, replyid)VALUES(";
+                    using (var conn = new NpgsqlConnection(connString))
+                    {
+                        conn.Open();
+
+                        // Insert some data
+                        using (var cmd = new NpgsqlCommand())
+                        {
+                            cmd.Connection = conn;
+                            cmd.CommandText = insert + tlMessage.Id.ToString() + "," + tlMessage.FromId + ",\'" + name_chat + "\'," + tlMessage.Date.ToString() + ",\'" + tlMessage.Message.Replace("'","\"") + "\'," + replyId.ToString() + ");";
+                            //cmd.Parameters.AddWithValue("p", "Hello world");
+                            try
+                            {
+                                NpgsqlDataReader reader = cmd.ExecuteReader();
+                            }
+                            catch(Exception e)
+                            {
+                                MessageBox.Show(e.Message);
+                            }
+                            conn.Close();
+
+                        }
+
+                    }
+
+                }
+            }
+
+        }
+
+
+        public async Task<TLChannelMessages> GetMessageFromChannelAsync(TLChannel channel, TelegramClient client)
+        {
+            TLChannelMessages res = await client.SendRequestAsync<TLChannelMessages>
+                (new TLRequestGetHistory()
+                {
+                    Peer = new TLInputPeerChannel { ChannelId = channel.Id, AccessHash = channel.AccessHash.Value },
+                    Limit = 400,
+                    AddOffset = 100,
+                    OffsetId = 0
+
+                });
+            return res;
+        }
+
+        public Dictionary<string, int> GetLastIdMessage(Dictionary<int, string> chats, string number)
+        {
+            System.IO.File.WriteAllText("D:\\" + number.Replace("+", "") + ".txt", string.Empty);
+            Dictionary<string, int> chat_lastid = new Dictionary<string, int>();
+            foreach (var chat in chats)
+            {
+                string select = "SELECT messageid FROM public.message where chatname=\'" + chat.Value + "\' order by messageid desc limit 1;";
+                using (var conn = new NpgsqlConnection(connString))
+                {
+                    conn.Open();
+
+                    // Insert some data
+                    using (var cmd = new NpgsqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = select;
+                        //cmd.Parameters.AddWithValue("p", "Hello world");
+                        NpgsqlCommand command = new NpgsqlCommand(select, conn);
+                        NpgsqlDataReader dr = command.ExecuteReader();
+                        bool flag = false;
+                        try
+                        {
+                            while (dr.Read())
+                            {
+                                flag = true;
+                                using (StreamWriter writetext = new StreamWriter("D:\\" + number.Replace("+", "") + ".txt", true))
+                                {
+                                    writetext.WriteLine(chat.Value+"|"+dr[0].ToString());
+                                }
+                                chat_lastid.Add(chat.Value, Convert.ToInt16(dr[0]));
+
+                            }
+                            if (flag == false)
+                            {
+                                using (StreamWriter writetext = new StreamWriter("D:\\" + number.Replace("+", "") + ".txt", true))
+                                {
+                                    writetext.WriteLine(chat.Value + "|" + "0");
+                                }
+                                chat_lastid.Add(chat.Value, 0);
+                            }
+
+                        }
+                        catch (Exception e)
+                        {
+                            return null;
+                        }
+
+                    }
+                }
+            }
+            return chat_lastid;
+        }
+        public int MaxReply(Dictionary<int,int> reply)
+        {
+         
+            int chatId = 0;
+            List<int> message_reply = new List<int>();
+            foreach(var temp in reply)
+            {
+                message_reply.Add(temp.Value);
+            }
+            var g = message_reply.GroupBy(i => i);
+            int max = 1;
+            foreach (var temp in g)
+            {
+                if (temp.Count<int>() > max)
+                {
+                    max = temp.Count<int>();
+                    chatId = temp.Key;
+                }
+            }
+            
+            return chatId;
+        }
+      
+        public string ReturnMessage(int Idmessage)
+        {
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+                  // Insert some data
+                    using (var cmd = new NpgsqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        string select = "SELECT message FROM public.message where messageid="+Idmessage.ToString()+";";
+                        NpgsqlCommand command = new NpgsqlCommand(select, conn);
+                        NpgsqlDataReader dr = command.ExecuteReader();
+
+                        while (dr.Read())
+                        {
+                            try
+                            {
+                            conn.Close();
+                            return dr[0].ToString();
+                            }
+                            catch
+                            {
+                            conn.Close();
+                            return null;
+                            }
+
+                        }
+                       
+
+                    
+                }
+
+
+            }
+            return null;
+        }
+        public Dictionary<string, string> GetReplyDictionary(Dictionary<string,int> chat_data)
+        {
+            Dictionary<string, string> repli_message = new Dictionary<string, string>();
+           
+            foreach (var chat in chat_data)
+            {
+                using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+              
+                    // Insert some data
+                    using (var cmd = new NpgsqlCommand())
+                    {
+                        cmd.Connection = conn;
+                       string select= "SELECT messageid, message, replyid FROM public.message where replyid<>-1 and chatname =\'" +chat.Key+ "\' and messageid>"+0.ToString()+"; ";
+                        NpgsqlCommand command = new NpgsqlCommand(select, conn);
+                        NpgsqlDataReader dr = command.ExecuteReader();
+                        Dictionary<int, int> messageId = new Dictionary<int, int>();
+                        while (dr.Read())
+                        {
+                            try
+                            {
+                                messageId.Add(Convert.ToInt16(dr[0]),Convert.ToInt16(dr[2]));
+                            }
+                            catch {
+                               break;
+                            }
+
+                        }
+                        if (messageId.Count != 0)
+                        {
+                            int maxReply = MaxReply(messageId);
+                            repli_message.Add(chat.Key, ReturnMessage(maxReply));
+                        }
+                        conn.Close();
+
+                    }
+                }
+               
+
+            }
+
+   
+
+            return repli_message;
+        }
+    }
+
+}
